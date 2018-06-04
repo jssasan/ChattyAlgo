513701340a32890cca612b527da8be621cb764ea
Jasmeet Sasan <jssasan@ilstu.edu>
2018-06-04 16:19:40 -0700

    this is jss branch

--- src/base/HelloWorld.java
+++ src/base/HelloWorld.java
@@ -5,9 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World! I am Jasmeet and excited to work with Katie Park!");
-		System.out.println("git is working fine! Let's try another thing!!!");
-		System.out.println("This is jSS branch commit!");
-		System.out.println("JSS another merge");
+		System.out.println("git is working!!");
+		System.out.println("testing push");
+		System.out.println("This is another branch jss");
 	}
 
 }
